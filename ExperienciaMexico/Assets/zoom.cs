7abf31951eb97e001469cfdb1fc2bba5094871c5
icecreamsoldier <mariana.9000@hotmail.com>
2020-05-03 20:29:05 -0500

    Fixed audio

--- ExperienciaMexico/Assets/zoom.cs
+++ ExperienciaMexico/Assets/zoom.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
-public class zoom : MonoBehaviour {
+public class zoom : MonoBehaviour, ITrackableEventHandler {
   public AudioClip[] aClips;
   public AudioSource audioSource;
+
+  private TrackableBehaviour mTrackableBehaviour;
 
   private RaycastHit hit;
   private bool mShowGUIButton = false;
@@ -15,8 +18,25 @@ public class zoom : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
-      audioSource.clip = aClips[3];
-      audioSource.Play();
+      mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+      if (mTrackableBehaviour) {
+        mTrackableBehaviour.RegisterTrackableEventHandler(this);
+      }
+    }
+
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus) {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED) {
+			           // Play audio when target is found
+                 audioSource.clip = aClips[3];
+                 audioSource.Play();
+        } else {
+			       // Stop audio when target is lost
+			       audioSource.Stop();
+        }
     }
 
     // Update is called once per frame
@@ -93,6 +113,7 @@ public class zoom : MonoBehaviour {
         for(int i = 0; i < 3; i++) {
           GameObject.Find(sarray[i]).transform.localScale = new Vector3(0.06717828f, 0.06717828f, 0.06717828f);
         }
+        audioSource.Stop();
         mShowGUIButton = false;
       }
     }
