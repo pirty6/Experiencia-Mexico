fe7921b52835791fafe7be902513a47fb5ee2b10
Luis Peña <A01364029@itesm.mx>
2020-05-03 20:35:46 -0500

    Teotihuacan model uploaded

---
+++ ExperienciaMexico/Assets/Scripts/DiaNoche.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DiaNoche : MonoBehaviour
+{
+    public Vector3 rotacionDia = new Vector3 (90, 180, 0);
+    public Vector3 rotacionNoche = new Vector3(45, -80, 0);
+    public float intensidadDia = 1;
+    public float intensidadNoche = 0.5f;
+    public Vector4 colorDia = new Vector4(255, 255, 255, 255);
+    public Vector4 colorNoche = new Vector4(225, 233, 255, 255);
+    Transform sol;
+    Light luzSol;
+    bool dia = true;
+    void AplicaCambio() {
+        
+        if (dia)
+        {
+            sol.rotation = Quaternion.Euler(90, 180, 0);
+            //luzSol.intensity = 0.01f;
+            luzSol.color = new Vector4(255, 255, 255, 255);
+
+        }
+        else
+        {
+            sol.rotation = Quaternion.Euler(45, -80, 0);
+           // luzSol.intensity = 0.001f;
+            luzSol.color = new Vector4(225, 233, 255, 255);
+
+        }
+
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        sol = GameObject.Find("Sol").GetComponent<Transform>();
+        luzSol = GameObject.Find("Sol").GetComponent<Light>();
+        AplicaCambio();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            dia = !dia;
+            AplicaCambio();
+        }
+    }
+}
---
+++ ExperienciaMexico/Assets/Scripts/SwitchNoche.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchNoche : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.GetComponent<Light>().enabled = false;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.GetComponent<Light>().enabled = true;
+        }
+    }
+}
